0b6de445c45ad9c04fb371ea8a2556df38fe7f28
Tarik Sahni <tariksahni@gmail>
2017-07-13 11:51:22 +0530

    Login Page Star Wars

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule} from '@angular/router';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }    from '@angular/http';
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    RouterModule.forRoot([
+      {
+        path:'login',
+        component: LoginComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/login/login.component.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core' ;
+import { Component, OnInit } from '@angular/core';
+import { Http,Response } from '@angular/http' ;
+import { Observable } from 'rxjs/Observable';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+@Injectable()
+export class LoginComponent implements OnInit {
+
+  constructor(private http: Http) { }
+
+  ngOnInit() {
+  }
+
+  login = (user) =>{
+    var username = user.username;
+    var password = user.password;
+    var user_api , user_birth ;
+    var url = 'https://swapi.co/api/people/?search='+username;
+    this.http.get(url).subscribe(data => { 
+      var count = data.json().count;
+      if( count == 0 )alert("NO such user");
+      if( count > 0 ){
+        user_api= (data.json().results[0].name);
+        user_birth = (data).json().results[0].birth_year;
+        if( password == user_birth && username == user_api ) alert("YOU CAN LOG IN ");
+        else if( password != user_birth || username != user_api) alert("CHECK Your Username and Passowrd");
+      }
+    })
+  }
+}
